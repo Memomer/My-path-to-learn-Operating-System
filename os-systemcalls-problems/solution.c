72e8c7a4e493e02717cdeab5e88ec8cda83bfd69
Mayank Kashyap <107757811+Memomer@users.noreply.github.com>
2023-01-26 23:40:51 +0530

    Create solution.c
    explanation

---
+++ os-systemcalls-problems/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid = fork();
+    if(pid<0){
+    fprintf(stderr,"Fork failed");
+    return 1;
+    }
+    if (pid == 0) {
+        int execlp("/bin/ps","ps", "-U", "0", NULL);
+    } else {
+        wait(NULL);
+        printf("Child Complete");
+    }
+    return 0;
+}
